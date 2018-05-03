7b854650464261ffc69b9eceec82747fc5300a05
Petya Djanovska <petya.djanovska@gmail.com>
2018-05-03 18:19:30 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,11 +1,3 @@
-class Dog
-  attr_accessor :name, :breed, :age
-
-  def initialize(name: name, breed: breed, age: age)
-    @name = name
-    @breed = breed
-    @age = age
-  end
-
+class Dog < ActiveRecord:Base
 
 end
